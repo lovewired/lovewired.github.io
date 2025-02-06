9bcdf878a8f2dac3eb18fd486ebc0fb7817f8808
꒱　 HOWLETT / JACKSON <143456066+lovewired@users.noreply.github.com>
2025-02-06 01:51:23 -0800

    h....testing.sn.sgne.

--- spfronters.js
+++ spfronters.js
@@ -1,13 +1,16 @@
-//this does not include custom fronts because im lazy and dont want to code that.
-//profile pictures have to be uploaded outside of simply plural for them to be shown. feel free to use ".fpfp {display:none;}" in your stylesheet to hide them or edit the html output directly
-
+// SimplyPlural Fronters by Teal In Tandem (nefola)
+// this program uses a simply plural user token to display who is fronting as some elements on a webpage, and you can style them hoever you want
+// if you find a fix to the double display on multifront bug, let me know. also yeah the code is a mess we know
 var fronters;
 var frontMembers = [];
 var imbed;
-var token = "0AMaZrQii2XWeOvn+I7xMEVRvD21Uipti33pgNPfqZWJ+rjrk3YoJXBe1rDjwX34" //replace this with a systems token. has to have read permissions enabled. this is the only thing you need to modify
+var token = 0AMaZrQii2XWeOvn+I7xMEVRvD21Uipti33pgNPfqZWJ+rjrk3YoJXBe1rDjwX34//replace this comment with your token, we recomend a write only token
 
+
+// api request that gets uid and menber id of all current fronters, ran first
 function getFronters(){
     var frontCheck = new XMLHttpRequest;
+
     frontCheck.addEventListener("load", useFronters);
     frontCheck.open("GET", "https://api.apparyllis.com:8443/v1/fronters/");
     frontCheck.setRequestHeader("Authorization", token);
@@ -15,45 +18,55 @@ function getFronters(){
 
 }
 
+//stores the data from the first api request and then initates a fronters to members
 function useFronters(){
     console.log(this)
     fronters = JSON.parse(this.responseText);
     console.log(fronters)
+    //console.log(fronters[0].content.member)
+
+    //getMembersData(fronters[0].content.uid, fronters[0].content.member)
     frontersToMembers();
 }
 
+// runs get member data for each fronter
 function frontersToMembers() {
     for (let i = 0; i < Object.keys(fronters).length; i++) {
         getMembersData(fronters[i].content.uid, fronters[i].content.member)
     }
+    // this code is bullshit 
 }
 
+// an api request for a spesific system member
 function getMembersData(uid, member){
     var getMembers = new XMLHttpRequest;
     getMembers.addEventListener("load", storeFronters);
     getMembers.open("GET", ("https://api.apparyllis.com:8443/v1/member/" + uid + "/" + member));
+    //getMembers.open("GET", ("https://api.apparyllis.com:8443/v1/member/"));
     getMembers.setRequestHeader("Authorization", token);
+
     getMembers.send();
+
 }
 
+//puts all the newly gathered fronter data(from getMembersData) into an object
 function storeFronters(){
-    console.log(this);
+    console.log(this)
+    //frontMembers = JSON.parse(this.responseText);
     frontMembers.push(JSON.parse(this.responseText));
-
-    frontMembers.sort((a, b) => a.content.name.localeCompare(b.content.name));
-
-    makeFronterElements(document.getElementById("spfronters"));
+    makeFronterElements(document.getElementById("SPF"));
 }
-
 function makeFronterElements(imbed) {
-    imbed.innerHTML = '';
-    for (let i = 0; i < frontMembers.length; i++) {
-        imbed.innerHTML += '<div class="frntr" "&' + frontMembers[i].content.name + '"><img class="fpfp" src="' + frontMembers[i].content.avatarUrl + '"><div class="nameprn"><div class="fname">' + frontMembers[i].content.name + '</div><div class="fprn">' + frontMembers[i].content.pronouns + '</div></div></div>';
-        console.log(i);
-        console.log(imbed.innerHTML);
+    for (let i = 0; i < Object.keys(fronters).length; i++) {
+
+        imbed.innerHTML += '<div class="mcont" "&' + frontMembers[i].content.name + '"><img class="favi" src="' + frontMembers[i].content.avatarUrl + '"><div class="fname">' + frontMembers[i].content.name + '</div><div class=fpro>' + frontMembers[i].content.pronouns + '</div></div>'
+        console.log(i)
+        console.log(imbed.innerHTML)
     }
+    console.log("makingem")
 }
 
 getFronters();
+
 
 console.log(Object.keys(frontMembers).length)
